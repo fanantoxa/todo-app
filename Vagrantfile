74845726efe1ec2a5157d804464f9405f90b037d
asivakov <anton.sivakov@ecommerce.com>
2016-01-12 17:11:04 +0200

    Updated provisoning

--- Vagrantfile
+++ Vagrantfile
@@ -15,6 +15,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
 
   config.vm.network :forwarded_port, guest: 300, host: 3000
+  config.vm.synced_folder('.','/vagrant')
   config.vm.provision :shell, path: "provision.sh"
 
   # Disable automatic box update checking. If you disable this, then
---
+++ provision.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Installing package dependencies"
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+sudo apt-get update
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+dpkg-reconfigure locales en
+
+sudo apt-get install -y git
+sudo apt-get install -y postgresql postgresql-client postgresql-contrib libpq-dev
+
+sudo pg_createcluster 9.3 main --start
+
+echo "Installing rvm, ruby and bundler"
+
+\curl -sSL https://get.rvm.io | bash
+source ~/.rvm/scripts/rvm
+
+rvm install 2.2.1
+
+gem install bundler
+
+echo "Configure app"
+
+echo "Creating postgres user:"
+
+sudo -u postgres bash -c "psql -c \"CREATE USER todo_user WITH PASSWORD 'testing';\""
+
+
+
+
+cd /vagrant
+
+
+echo "Installing gems"
+
+bundle install
+
+echo "Running migrations"
+
+bundle exec rake db:create:all --trace
+bundle exec rake db:migrate --trace
+bundle exec rake db:migrate --trace RAILS_ENV=test
